e9eb2c3e1a83aa7181527297af525fb679032b00
popbal <lukybrtek@gmail.com>
2025-12-09 01:17:56 +0100

    created a monster cuz nobody wants to see marshall no more im chopped liver

---
+++ popbal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class popbal {
+    public static void main(String[] args) {
+        System.out.println("cs");
+        Scanner input = new Scanner(System.in);
+        String odpoved = input.next();
+        System.out.println(odpoved);
+        input.close();
+    }
+}
